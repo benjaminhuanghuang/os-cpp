f4d7cded7732ce15e663a0bec4c62783e53a9394
benjamin huang <benjaminhuanghuang@gmail.com>
2022-08-02 18:34:58 -0700

    boot from image

--- 01-hello-os/kernel.cpp
+++ 01-hello-os/kernel.cpp
@@ -1,24 +1,28 @@
-void printf(char * str){
-  unsigned short*videoMem = (unsigned short*) 0xb8000;
-  for(int i=0; str[i]; i++){
-    videoMem[i]= (videoMem[i] & 0xFF00) | str[i];
+void printf(char *str)
+{
+  unsigned short *videoMem = (unsigned short *)0xb8000;
+  for (int i = 0; str[i]; i++)
+  {
+    videoMem[i] = (videoMem[i] & 0xFF00) | str[i];
   }
 }
-
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 
-extern "C" void callConstructors() {
-    for (constructor* i = &start_ctors; i != &end_ctors; i++) {
-        (*i)();
-    }
+extern "C" void callConstructors()
+{
+  for (constructor *i = &start_ctors; i != &end_ctors; i++)
+  {
+    (*i)();
+  }
 }
 
+extern "C" void kernelMain()
+{
+  printf((char *)"hellO OOOOOOS!!!");
 
-extern "C" void kernelMain(){
-  printf((char*)"hello");
-
-  while(1);
+  while (1)
+    ;
 }
